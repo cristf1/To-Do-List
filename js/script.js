ead3acb0232d607f38c65e945117f72b7bcc1330
cristf1 <cristf1@users.noreply.github.com>
2022-08-22 14:14:42 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,33 @@
+function newItem() {
+    //javascript
+    //1. Adding a new item to the list of items:
+    let list = $('<li></li>');
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton
+      .append(document.createTextNode('X'))
+      .addClass('crossOutButton');
+    $('#input').each(function (i) {
+      let inputValue = $('#input').val();
+      list.append(inputValue).append(crossOutButton);
+      if (inputValue.length == 0) {
+        console.log('You must write something!');
+      } else {
+        $('#list').append(list);
+      }
+    });
+    //2. Crossing out an item from the list of items:
 
-function newItem(){
+    list.on('dblclick', function () {
+      $(this).toggleClass('strike');
+    });
 
-//javascript
-//1. Adding a new item to the list of items:
-  let list = $('<li></li>');
-  let crossOutButton = $('<crossOutButton></crossOutButton>');
-  crossOutButton.append(document.createTextNode('X')).addClass('crossOutButton');
-  $('#input').each(function(i){
-    let inputValue = $('#input').val();
-     list.append(inputValue).append(crossOutButton);
-    if(inputValue.length == 0){
-      console.log('You must write something!')
-    }else{
-      $('#list').append(list);
-    };
-  })}
+    //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+    $('.crossOutButton').on('click', deleteListItem);
 
-
- //2. Crossing out an item from the list of items:
-
-list.on('dblclick', function(){
-  $(this).toggleClass('strike');
-});
-
- //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-  $('.crossOutButton').on('click',deleteListItem)
-
-  function deleteListItem(){
-    console.log('delete');
-  $(list).addClass('delete');
-  };
- // 4. Reordering the items:
-   $('#list').sortable();
+    function deleteListItem() {
+      console.log('delete');
+      $(this).parent().addClass('delete');
+    }
+    // 4. Reordering the items:
+    $('#list').sortable();
+  }
